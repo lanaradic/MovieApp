516473cd4ee01f2e5d9ced1003d99bd64cb43c5c
Lana Radic <lanaradic1990@gmail.com>
2023-08-08 11:52:11 +0200

    main-layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css';
+import './App.scss';
 import React from 'react';
+import MainLayout from './layout/MainLayout';
 function App() {
   return (
-    <>
+    <MainLayout>
       <div>test</div>
-    </>
+    </MainLayout>
   );
 }
 
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import SideBar from '../components/SideBar';
+import './styles/index.scss';
+import '../styles/colors.scss';
+
+const MainLayout = ({ children }) => {
+  return (
+    <>
+      <div className="main-wrapper">
+        <aside className="side-bar">
+          <SideBar></SideBar>
+        </aside>
+        <main clasName="main-content">{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
